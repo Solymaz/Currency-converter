6498921af8cc4fee5cbc13144bd999b6d01177b0
Solymaz <44635076+Solymaz@users.noreply.github.com>
2021-03-29 21:12:28 +0200

    showed the currencies names instead of the codes

--- src/converter/converter.tsx
+++ src/converter/converter.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 import {
   AmountHeader,
@@ -10,20 +10,75 @@ import {
   Amount,
 } from "./styled";
 
-export default function CurrencyConverter() {
+type TCurrenciesCodeRate = {
+  [key: string]: number;
+};
 
-  //fetch the currency data on load 
+type TCountriesDetails = {
+  code: string;
+  name: string;
+  symbol: string;
+};
+
+type TCurrenciesDetails = {
+  code: string;
+  name: string;
+  rate: number;
+};
+
+export default function CurrencyConverter() {
+  const [
+    currenciesCodeRate,
+    setCurrenciesCodeRate,
+  ] = useState<TCurrenciesCodeRate>({});
+  const [countriesDetails, setCountriesDetails] = useState<TCountriesDetails[]>(
+    []
+  );
+  const [currenciesDetails, setCurrenciesDetails] = useState<
+    TCurrenciesDetails[]
+  >([]);
+
+  //fetch the currency data on load and save it from the results
   useEffect(() => {
     const currenciesApiUrl = `http://data.fixer.io/api/latest?access_key=0e99bcf06781539a85115ed4a10a18fe&format=1`;
-    axios.get(currenciesApiUrl);
+    const countriesApiUrl = `https://restcountries.eu/rest/v2/all`;
+    axios.get(currenciesApiUrl).then((result) => {
+      setCurrenciesCodeRate(result.data.rates);
+    });
+    axios.get(countriesApiUrl).then((result) => {
+      const currencies = result.data
+        .map((country) => country.currencies)
+        .flat();
+      setCountriesDetails(currencies);
+    });
   }, []);
+
+  //combine the currency code with currency name and the rate
+  useEffect(() => {
+    if (currenciesCodeRate && countriesDetails) {
+      setCurrenciesDetails(
+        Object.entries(currenciesCodeRate).map(([code, rate]) => {
+          const currency = countriesDetails.find(
+            (country) => country.code === code
+          );
+          return {
+            code: code,
+            name: (currency && currency.name) || code,
+            rate: rate,
+          };
+        })
+      );
+    }
+  }, [currenciesCodeRate, countriesDetails]);
 
   return (
     <Converter>
       <CurrencyDetails>
         <CurrencyHeader>Currency I Have:</CurrencyHeader>
         <Currencies>
-          <Currency></Currency>
+          {currenciesDetails.map((currencyDetail) => (
+            <Currency key={currencyDetail.code}>{currencyDetail.name}</Currency>
+          ))}
         </Currencies>
         <AmountHeader>Amount:</AmountHeader>
         <Amount></Amount>
