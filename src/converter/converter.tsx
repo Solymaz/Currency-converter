1eb088b82761ae411a067cfa0e2a9660b5e6d4d7
Solymaz <44635076+Solymaz@users.noreply.github.com>
2021-03-29 18:36:19 +0200

    added currency API call

--- src/converter/converter.tsx
+++ src/converter/converter.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useEffect } from "react";
+import axios from "axios";
 import {
   AmountHeader,
   CurrencyHeader,
@@ -10,6 +11,13 @@ import {
 } from "./styled";
 
 export default function CurrencyConverter() {
+
+  //fetch the currency data on load 
+  useEffect(() => {
+    const currenciesApiUrl = `http://data.fixer.io/api/latest?access_key=0e99bcf06781539a85115ed4a10a18fe&format=1`;
+    axios.get(currenciesApiUrl);
+  }, []);
+
   return (
     <Converter>
       <CurrencyDetails>
